578b697bfa8d269afa5010bd8cc57a5503c7f2d3
yann-morpheus <yannducoeur@gmail.com>
2024-09-06 16:17:08 +0200

    first release

---
+++ readme.txt
@@ -0,0 +1,22 @@
+
+              manuel d'utilsation de segmentor
+========================================================================
+
+  1> si vos fichier doivent avoir une entete ou un pieds de page fixe 
+
+	-saisissez l'entete dans le fichier <header.txt>.
+	-saisissez le pieds de page  dans le fichier <footer.txt>.
+
+  2> si vos fichier n'ont pas une entete ou un pieds de page fixe  vous pouver laisser ces fichiers vides.
+  3> deplacer le fichier a segmenter dans le repertoire parent ou se situe le l'executable de segmentor.
+  4> lancer l'executable puis suiver les instructions...patienter quelques seconde  et vos segmentation sont pretes.
+
+	    regles d'utilisation 
+  ====================================================
+  1> ne pas supprimer les fichiers header.txt et footer.txt.
+
+
+=================================================================================================================
+************  si vous avez apprecieer cet outils n'hesiter pas a me suivre sur github yann-morpheus *************
+=================================================================================================================
+	
--- segmentor1.0.c
+++ segmentor1.0.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 
 
-
+//fonction de segmentation
 int segmentText(char nameOfRepositery [255],int numberOfLinePerFile,char FileName[255],char extension [4]){
 
 char lineText [1024];
@@ -20,14 +20,18 @@ FILE *file=fopen(fullFilename,"r+");
 while(fgets(lineText,sizeof(lineText),file)!=NULL){
 
     if(canCreateFile){
+            // initialisation de chaque fichier segment
         snprintf(fullFileNameOfSegmentFile,255,"%s/%s%d.%s",nameOfRepositery,FileName,fileNumber++,extension);
         segmentFile= fopen(fullFileNameOfSegmentFile,"w");
+        // insertion d'un entete  a chaque ouverture du fichier
         AddHeader(segmentFile);
         canCreateFile=false;
     }else if((numberOfLine%numberOfLinePerFile)!=0){
+        // insertion des donnes dans chaque fichier segment
             fprintf(segmentFile,lineText);
 
         }else{
+            // insertion du pieds de page a la fin de l'insertion des donnees et fermeture du fichier
             Addfooter(segmentFile);
             fclose(segmentFile);
             canCreateFile=true;
@@ -37,6 +41,7 @@ while(fgets(lineText,sizeof(lineText),file)!=NULL){
 
 
 };
+//messsage de fin de traitement et fermeture du fichier d'origine
 printf("\n  %d fichiers  ont ete creer avec succes ",fileNumber);
 fclose(file);
 return 0;
@@ -60,17 +65,36 @@ int Addfooter(FILE *segmentfile){
         fprintf(segmentfile,footerText);
     }
 }
+//creation du dossier qui contiendras les fichier
  void createRepository(char nameOfRepository[255]){
     mkdir(nameOfRepository);
 }
 
+
  int main(void){
+
+    printf("==========================================================================================================================================\n");
+    printf("**********    **********    **********     ****         ***    **********   ***         *   **************  ***********   ***********\n");
+    printf("*             *             *              *   *       *  *    *            *  *        *          *        *         *   *           *\n");
+    printf("*             *             *              *    *     *   *    *            *   *       *          *        *         *   *           *\n");
+    printf("*             *             *              *     *   *    *    *            *    *      *          *        *         *   *          *\n");
+    printf("**********    **********    *              *       *      *    **********   *     *     *          *        *         *   ***********\n");
+    printf("         *    *             *    *****     *              *    *            *      *    *          *        *         *   *         *\n");
+    printf("         *    *             *    *   *     *              *    *            *       *   *          *        *         *   *          *\n");
+    printf("         *    *             *        *     *              *    *            *        *  *          *        *         *   *           *\n");
+    printf("**********    **********    **********     *              *    **********   *         ***          *        ***********   *            *\n");
+    printf("==========================================================================================================================================\n");
+    printf("\n\n==========================================================================================================================================\n");
+    printf(" segmentor v1.0 est un outils permetant de segmenter un fichier html,txt,docx,php,css,js.... en plusieurs sous fichier independant \n"
+           "\n\n");
+    printf("==========================================================================================================================================\n");
+
      char nameOfRepositery [255];
      int numberOfLinePerFile;
      char nameOfSegmentFile[255];
      char FileName[255];
      char extension [4];
-     // recuperation du nombre de ligne par segment
+     // recuperation des choix du user par rapport au segment
      printf("veuiller saisir le nom du dossier dans lequel doit se situer les fichier: ");
      scanf("%s",& nameOfRepositery);
      printf("entrer le nom du fichier ( sans l'extension ):");
@@ -80,10 +104,12 @@ int Addfooter(FILE *segmentfile){
      printf("entrer le nombre de ligne a obtenir par fichier :");
      scanf("%d",&numberOfLinePerFile);
 
-
+        // execution
      createRepository( nameOfRepositery);
      segmentText(nameOfRepositery,numberOfLinePerFile,FileName,extension);
-     printf("\n\n merci d'avoir utiliser segmentor v1.0 concu par yann-morpheus Github:yann-morpheus");
+     printf("\n\n merci d'avoir utiliser segmentor v1.0 concu par yann-morpheus Github:yann-morpheus\n");
+   printf("==========================================================================================================================================\n");
+
  return 0;
  }
 
