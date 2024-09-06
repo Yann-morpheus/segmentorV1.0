940840bd8f08701b47a3a66be7146c994acb5e2d
yann-morpheus <yannducoeur@gmail.com>
2024-09-06 15:06:09 +0200

    premiere version'

--- segmentor1.0.c
+++ segmentor1.0.c
@@ -1,240 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-//fonction qui est charger de recuperer l'entete dans le fichier header.txt
-
-void getHeader(void){
-char headerTxt[1024];
-FILE *header=fopen("header.txt","r+");
-//lecture de toute le fichier
-while((fgets(headerTxt,1024,header))!=NULL){
-
-    //attribution de l'entete aux fichiers
-
-};
-}
-// recupere le footer dans un fichier txt
-void getFooter(void){
-char footerTxt[1024];
-FILE *footer=fopen("footer.txt","r+");
-//lecture de toute le fichier
-while((fgets(footerTxt,1024,footer))!=NULL){
-
-    //attribution des pieds de pages aux fichiers
-
-};
-}
-int getUserSegmentNumber(void){
-    int lines;
-printf("veuiller saisir le nombre de lignes a segmenter dans le fichiers");
-scanf("%d",lines);
-lines =0;
-return lines;
-}
-
-void getUserfilesAndSegment(){
+#include <stdbool.h>
 
 
-//gestion de l'erreur au cas ou path >256
 
-FILE *fileToSegment=fopen("StaticDataValeurDepenseBudget.txt","w+");
-// inactive section
-char fileTxtPerLine[1024];
-int line=0;
-int indexOfSegment=1;
-char nameOfFile[255];
-//inactive section
-//lecture de toute le fichier
-while((fgets(fileTxtPerLine,1024,fileToSegment))!=NULL){
-line++;
+int segmentText(char nameOfRepositery [255],int numberOfLinePerFile,char FileName[255],char extension [4]){
 
-if ((line%5000)==0){
-        indexOfSegment++;
+char lineText [1024];
+int numberOfLine=1;
+int fileNumber=1;
+char fullFilename [255];
+bool canCreateFile=true;
+FILE *segmentFile= NULL;
+char fullFileNameOfSegmentFile[255];
+snprintf(fullFilename,255,"%s.%s",FileName,extension);
+FILE *file=fopen(fullFilename,"r+");
 
+while(fgets(lineText,sizeof(lineText),file)!=NULL){
 
-    //creation du nouveau fichier
-snprintf(nameOfFile,255,"StaticDataValeurDepenseBudget%d.txt",indexOfSegment);
+    if(canCreateFile){
+        snprintf(fullFileNameOfSegmentFile,255,"%s/%s%d.%s",nameOfRepositery,FileName,fileNumber++,extension);
+        segmentFile= fopen(fullFileNameOfSegmentFile,"w");
+        AddHeader(segmentFile);
+        canCreateFile=false;
+    }else if((numberOfLine%numberOfLinePerFile)!=0){
+            fprintf(segmentFile,lineText);
 
-FILE *segment=fopen(nameOfFile,"w");
+        }else{
+            Addfooter(segmentFile);
+            fclose(segmentFile);
+            canCreateFile=true;
+        };
 
-fprintf(segment,fileTxtPerLine);
-fclose(segment);
-
-
+        numberOfLine++;
 
 
 };
+printf("\n  %d fichiers  ont ete creer avec succes ",fileNumber);
+fclose(file);
+return 0;
+ }
 
-};
-fclose(fileToSegment);
+ // insertion d'un header sur toutes les pages
 
-printf("l'operation de segementaion est achever avec succes avons obtenue : %d fichier segmenter" , indexOfSegment);
+int AddHeader(FILE *segmentfile){
+    char headerText[1024];
+    FILE *header=fopen("header.txt","r");
+    while(fgets(headerText,sizeof(headerText),header)!=NULL){
+        fprintf(segmentfile,headerText);
 
-
+    }
 }
-//fonction principale
-int main(){
-    getHeader();
-    getFooter();
-
-    printf("1");
-    getUserfilesAndSegment();
+// fonction qui permet d'inserer un footer sur toutes les pages
+int Addfooter(FILE *segmentfile){
+    char footerText[1024];
+    FILE *footer=fopen("footer.txt","r");
+    while(fgets(footerText,sizeof(footerText),footer)!=NULL){
+        fprintf(segmentfile,footerText);
+    }
+}
+ void createRepository(char nameOfRepository[255]){
+    mkdir(nameOfRepository);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ int main(void){
+     char nameOfRepositery [255];
+     int numberOfLinePerFile;
+     char nameOfSegmentFile[255];
+     char FileName[255];
+     char extension [4];
+     // recuperation du nombre de ligne par segment
+     printf("veuiller saisir le nom du dossier dans lequel doit se situer les fichier: ");
+     scanf("%s",& nameOfRepositery);
+     printf("entrer le nom du fichier ( sans l'extension ):");
+     scanf("%s",&FileName);
+     printf("entrer l'extension du fichier (sans le '.') :");
+     scanf("%s",&extension);
+     printf("entrer le nombre de ligne a obtenir par fichier :");
+     scanf("%d",&numberOfLinePerFile);
+
+
+     createRepository( nameOfRepositery);
+     segmentText(nameOfRepositery,numberOfLinePerFile,FileName,extension);
+     printf("\n\n merci d'avoir utiliser segmentor v1.0 concu par yann-morpheus Github:yann-morpheus");
+ return 0;
+ }
 
 
